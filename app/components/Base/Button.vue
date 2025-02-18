738f0eb566e2d2529db2b046922af1988218c8bb
Pylyp Borysov <developer.frontco@gmail.com>
2025-02-18 20:19:26 +0200

    feat: add base components

---
+++ app/components/Base/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    class="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 transition disabled:bg-gray-400 disabled:cursor-not-allowed"
+    :type="props.type || 'button'"
+    :disabled="props.disabled"
+    @click="emit('click', $event)"
+  >
+    <slot>
+      {{ props.title }}
+    </slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  type?: HTMLButtonElement["type"];
+  title?: string;
+  disabled?: boolean;
+};
+
+type Emit = {
+  (eventName: "click", event: Event): void;
+};
+
+const props = defineProps<Props>();
+const emit = defineEmits<Emit>();
+</script>
---
+++ app/components/Base/input.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="flex items-center space-x-2 border border-gray-300 bg-white rounded-lg px-4 py-2 focus-within:ring-2 focus-within:ring-blue-500"
+    :class="props.wrapperClass"
+  >
+    <input
+      v-model="modelValue"
+      v-bind="$attrs"
+      :type="props.type"
+      :disabled="props.disabled"
+      :placeholder="placeholder"
+      class="flex-1 bg-transparent focus:outline-none text-gray-700"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  type: string;
+  disabled?: boolean;
+  placeholder?: string;
+  wrapperClass?: string | string[];
+};
+
+const props = defineProps<Props>();
+const modelValue = defineModel<string>();
+</script>
